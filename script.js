2c12455acecd7dbed7023112f25a7ae37f7f48fd
Rashawn Raiford <raiford87@gmail.com>
2020-10-15 20:52:09 -0400

    Added code to get superhero from superhero api

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
-    
 
-function superhero() {
-    var queryURL = "https://superheroapi.com/api.php/10164273699360858/search/batman"
+  //Number entered by user.
+  //4 is just a placeholder until we create the input and get the value.
+  var userNumber = 4;
+
+  //Function to get the superhero from the superhero api by id
+  //Must be between 1 and 731
+  function getSuperHero(id) {
+    var queryURL = "https://superheroapi.com/api.php/10164273699360858/" + id;
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-        console.log(response)
+      console.log(response);
+      var name = response.name;
+
     });
   }
-superhero()
-})
+
+  getSuperHero(userNumber);
+
+});
