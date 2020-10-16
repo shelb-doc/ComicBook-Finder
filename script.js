8296879c6d6fe07f30e583ab3749443226006b53
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-15 20:46:28 -0400

    added ajax call got goodreads and comments for organization in js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    
+  // JS VARIABLES
 
-function superhero() {
+  // FUNCTION DEFINITIONS
+  function superhero() {
     var queryURL = "https://superheroapi.com/api.php/10164273699360858/search/batman"
     $.ajax({
       url: queryURL,
@@ -10,5 +11,21 @@ function superhero() {
         console.log(response)
     });
   }
-superhero()
+
+  function goodReads() {
+    var queryURL = "https://v1.nocodeapi.com/shelboc/gr/dIBrccmAYkfwiAFv/search?q=spiderman"
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+        console.log(response)
+    });
+  }
+
+  // FUNCTION CALLS
+  goodReads()
+  superhero()
+
+  // EVENT LISTENERS
+
 })
