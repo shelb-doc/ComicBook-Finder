56c36500fe01a9bd65865405c7480f6e85d6c341
Rashawn Raiford <raiford87@gmail.com>
2020-10-16 22:04:44 -0400

    Updated script to populate page with superhero data

--- script.js
+++ script.js
@@ -7,75 +7,62 @@
 //===this will be remove by chris, just in here for testing API data call =================
 
 $(document).ready(function () {
-  $("#myModal").modal();
-  function superhero() {
-    var queryURL =
-      "https://superheroapi.com/api.php/10164273699360858/search/batman";
+
+  //VARIABLES
+
+  //Number entered by user.
+  //4 is just a placeholder until we create the input and get the value.
+  var userNumber = Math.floor(Math.random() * 732);
+
+  //FUNCTIONS
+
+  //Function to get the superhero from the superhero api by id
+  //Must be between 1 and 731
+  function getSuperHero(id) {
+    var queryURL = "https://superheroapi.com/api.php/10164273699360858/" + id;
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
       console.log(response);
-      console.log(response.results[0].image.url);
-      console.log(response.results[0].biography);
-      var image = document.getElementById("characterimage");
-      var imageURL = response.results[0].image.url;
-      var name = response.results[0].name;
-
-      //Variable need for the Character Details
-      var description = response.results[0].biography;
-
-      $("#characterimage").append(
-        "<img id='image' src=" + imageURL + "></img>"
-      );
+      var name = response.name;
+      var heroImage = response.image.url;
       $("#characterName").append("<h1>" + name + "</h1>");
-      $("#characterDetails").append("<p>" + description + "</p>");
-      $("#characterDetails").append("<p>" + appearance + "</p>");
-      $("#bookimage").append("<img src='backgroundinspo.png'></img>");
+      $('#characterimage').append('<img id="image" src="' + heroImage + '"></img>');
+
+      var aliases = response.biography.aliases;
+      var aliasesText = "Aliases: ";
+
+      for(var i = 0; i < aliases.length; i++){
+          aliasesText += (i < aliases.length - 1) ? aliases[i] + ', ' : aliases[i];       
+      }
+
+      $('#characterDetails').append('<p>Full Name: ' + response.biography['full-name'] +  '</p>');
+      $('#characterDetails').append('<p>' + aliasesText + '</p>');
+      $('#characterDetails').append('<p>Place of Birth: ' + response.biography['place-of-birth'] + '</p>');
+      $('#characterDetails').append('<p>First Appearance: ' + response.biography['first-appearance'] + '</p>');
+      $('#characterDetails').append('<p>Publisher: ' + response.biography.publisher + '</p>');
+
+      getBooks(name);
     });
   }
-  superhero();
+
+  //Function to get books and comics related to the superhero from goodreads api
+  function getBooks(name) {
+    var queryURL =
+      "https://v1.nocodeapi.com/shelboc/gr/dIBrccmAYkfwiAFv/search?q=" + name;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var books = response.results;
+    });
+  }
+
+  // FUNCTION CALLS
+  $("#myModal").modal();
+  getSuperHero(userNumber);
+
+  // EVENT LISTENERS
 });
-
-//=========================================================================================
-
-//VARIABLES
-
-//Number entered by user.
-//4 is just a placeholder until we create the input and get the value.
-var userNumber = 4;
-
-//FUNCTIONS
-
-//Function to get the superhero from the superhero api by id
-//Must be between 1 and 731
-function getSuperHero(id) {
-  var queryURL = "https://superheroapi.com/api.php/10164273699360858/" + id;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var name = response.name;
-
-    getBooks(name);
-  });
-}
-
-//Function to get books and comics related to the superhero from goodreads api
-function getBooks(name) {
-  var queryURL =
-    "https://v1.nocodeapi.com/shelboc/gr/dIBrccmAYkfwiAFv/search?q=" + name;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var books = response.results;
-  });
-}
-
-// FUNCTION CALLS
-getSuperHero(userNumber);
-
-// EVENT LISTENERS
